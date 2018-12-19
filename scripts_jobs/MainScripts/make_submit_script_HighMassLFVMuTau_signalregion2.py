f1bf1503ccd77d4d636808b9c32198bb64fb1dd6
Diego Beghin <diego.beghin@gmail.com>
2018-12-19 15:52:18 +0100

    initialising day-to-day development

--- scripts_jobs/MainScripts/make_submit_script_HighMassLFVMuTau_signalregion2.py
+++ scripts_jobs/MainScripts/make_submit_script_HighMassLFVMuTau_signalregion2.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         #command1 = command1 + "export workdir=/user/dbeghin/Work/MuTauHighMass/ " + "\n"  #Set your working directory, where the code is located
         #command1 = command1 + "cd $workdir"
         outFile.write(command1)
-        command3 = "qsub -q localgrid@cream02 -o " + "../out_err/"+myname[jj] +".stdout -e " +"../out_err/"+myname[jj] +".stderr -l walltime=15:00:00    " + "../Jobs_to_submit/"+name_out + "\n"  #Command to submit one job to the localgrid
+        command3 = "qsub -q localgrid@cream02 -o " + "../out_err/"+myname[jj] +".stdout -e " +"../out_err/"+myname[jj] +".stderr -l walltime=07:00:00    " + "../Jobs_to_submit/"+name_out + "\n"  #Command to submit one job to the localgrid
         submit_File.write(command3)
         ligne=0
 
@@ -64,7 +64,7 @@ if __name__ == "__main__":
                 command1 = command1 + "mkdir Reweighting" + "\n"
                 command1 = command1 + "cp /user/dbeghin/Work/MuTauHighMass/Reweighting/*.root Reweighting/" + "\n"
                 outFile.write(command1)
-                command3 = "qsub -q localgrid@cream02 -o "+ "../out_err/"+scr_name +".stdout -e " +"../out_err/"+scr_name +".stderr -l walltime=15:00:00    " + "../Jobs_to_submit/"+scr_name+".sh" + "\n"
+                command3 = "qsub -q localgrid@cream02 -o "+ "../out_err/"+scr_name +".stdout -e " +"../out_err/"+scr_name +".stderr -l walltime=07:00:00    " + "../Jobs_to_submit/"+scr_name+".sh" + "\n"
                 submit_File.write(command3)
             #Below, the command to execute your code, use the correct syntax for your own code, with all arguments (file in, file out, etc.) in the proper order
             #Note that i[0:-1] is just the name of the root file in /pnfs
